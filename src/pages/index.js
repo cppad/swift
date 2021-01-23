58ef0bbc97fdc392ca18c17f4085f1c75612fb4b
Michelle Lee <michellechulee@gmail.com>
2021-01-22 17:36:42 -0800

    added section templates

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,20 +54,140 @@ function Feature({imageUrl, title, description}) {
   );
 }
 
+
+
 function News(){
   return(
-    <div class="row">
-      <div class="col col--6">col</div>
-      <div class="col col--6">
-        <a class="twitter-timeline" data-lang="en" 
-          data-width="300" data-height="300" 
-          data-theme="dark" 
-          href="https://twitter.com/calpolyswift?ref_src=twsrc%5Etfw">
-            Tweets by calpolyswift</a>
-        <script async src="https://platform.twitter.com/widgets.js" 
-          charset="utf-8"></script>
+    <section class="news">
+      <h2 class="news">Recent News</h2>
+      <div class="row">
+        <div class="col col--4">
+          <div class="card-demo">
+            <div class="card">
+              <div class="card__image">
+                <img
+                  src="https://images.unsplash.com/photo-1506624183912-c602f4a21ca7?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+                  alt="Image alt text"
+                  title="Logo Title Text 1"
+                />
+              </div>
+              <div class="card__body">
+                <h4>Quaco Lighthouse</h4>
+                <small>
+                  The Quaco Head Lighthouse is a well maintained lighthouse close to St.
+                  Martins. It is a short, beautiful walk to the lighthouse along the
+                  seashore.
+                </small>
+              </div>
+              <div class="card__footer">
+                <button class="button button--primary button--block">Visit</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col col--4">
+          <div class="card-demo">
+            <div class="card">
+              <div class="card__image">
+                <img
+                  src="https://images.unsplash.com/photo-1506624183912-c602f4a21ca7?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+                  alt="Image alt text"
+                  title="Logo Title Text 1"
+                />
+              </div>
+              <div class="card__body">
+                <h4>Quaco Lighthouse</h4>
+                <small>
+                  The Quaco Head Lighthouse is a well maintained lighthouse close to St.
+                  Martins. It is a short, beautiful walk to the lighthouse along the
+                  seashore.
+                </small>
+              </div>
+              <div class="card__footer">
+                <button class="button button--primary button--block">Visit</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col col--4">
+          <a class="twitter-timeline" data-lang="en" 
+            data-width="300" data-height="300" 
+            data-theme="dark" 
+            href="https://twitter.com/calpolyswift?ref_src=twsrc%5Etfw">
+              Tweets by calpolyswift</a>
+          <script async src="https://platform.twitter.com/widgets.js" 
+            charset="utf-8"></script>
+        </div>
       </div>
-    </div>
+    </section>
+  )
+}
+
+function TwoColumns1() {
+  return(
+    <section class="">
+      <h2 class="news">Header2</h2>
+      <div class="row">
+        <div class="col col--6">
+          <p>Leverage agile frameworks to provide a robust synopsis for high 
+            level overviews. Iterative approaches to corporate strategy foster 
+            collaborative thinking to further the overall value proposition. 
+            Organically grow the holistic world view of disruptive innovation 
+            via workplace diversity and empowerment.</p>
+          <p>Bring to the table win-win survival strategies to ensure proactive 
+            domination. At the end of the day, going forward, a new normal that 
+            has evolved from generation X is on the runway heading towards a 
+            streamlined cloud solution. User generated content in real-time will 
+            have multiple touchpoints for offshoring.</p>
+          <p>Capitalize on low hanging fruit to identify a ballpark value added 
+            activity to beta test. Override the digital divide with additional 
+            clickthroughs from DevOps. Nanotechnology immersion along the 
+            information highway will close the loop on focusing solely on the 
+            bottom line.</p>
+        </div>
+        <div class="col col--6">
+          <img
+            src="https://images.unsplash.com/photo-1506624183912-c602f4a21ca7?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+            alt="Image alt text"
+            title="Logo Title Text 1"
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+function TwoColumns2() {
+  return(
+    <section class="">
+      <h2 class="news">Header2</h2>
+      <div class="row">
+        <div class="col col--6">
+          <img
+            src="https://images.unsplash.com/photo-1506624183912-c602f4a21ca7?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+            alt="Image alt text"
+            title="Logo Title Text 1"
+          />
+        </div>
+        <div class="col col--6">
+          <p>Leverage agile frameworks to provide a robust synopsis for high 
+            level overviews. Iterative approaches to corporate strategy foster 
+            collaborative thinking to further the overall value proposition. 
+            Organically grow the holistic world view of disruptive innovation 
+            via workplace diversity and empowerment.</p>
+          <p>Bring to the table win-win survival strategies to ensure proactive 
+            domination. At the end of the day, going forward, a new normal that 
+            has evolved from generation X is on the runway heading towards a 
+            streamlined cloud solution. User generated content in real-time will 
+            have multiple touchpoints for offshoring.</p>
+          <p>Capitalize on low hanging fruit to identify a ballpark value added 
+            activity to beta test. Override the digital divide with additional 
+            clickthroughs from DevOps. Nanotechnology immersion along the 
+            information highway will close the loop on focusing solely on the 
+            bottom line.</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
@@ -116,7 +236,7 @@ function Home() {
       
       
       <main>
-        {features && features.length > 0 && (
+        {/* {features && features.length > 0 && (
           <section className={styles.features}>
             <div className="container">
               <div className="row">
@@ -126,11 +246,23 @@ function Home() {
               </div>
             </div>
           </section>
-        )}
+        )} */}
 
-        <section class="container">
-          <News />
-        </section>
+        <div class="section">
+          <section class="container">
+            <News />
+          </section>
+        </div>
+        <div class="section section-background-color">
+          <section class="container">
+            <TwoColumns1 />
+          </section>
+        </div>
+        <div class="section">
+          <section class="container">
+            <TwoColumns2 />
+          </section>
+        </div>
 
       </main>
     </Layout>
