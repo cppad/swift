d488884922c3649192109f62225793b34b249234
Michelle Lee <michellechulee@gmail.com>
2021-01-26 23:41:25 -0800

    text style changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
-function News(){
-  return(
+function News() {
+  return (
     <section class="news">
       <h2 class="news">Latest Activity</h2>
       <div class="row">
@@ -24,14 +24,16 @@ function News(){
               <div class="card__body">
                 <h4>Quaco Lighthouse</h4>
                 <small>
-                  The Quaco Head Lighthouse is a well maintained lighthouse close to St.
-                  Martins. It is a short, beautiful walk to the lighthouse along the
-                  seashore. The Quaco Head Lighthouse is a well maintained lighthouse close to St.
-                  Martins.
+                  The Quaco Head Lighthouse is a well maintained lighthouse
+                  close to St. Martins. It is a short, beautiful walk to the
+                  lighthouse along the seashore. The Quaco Head Lighthouse is a
+                  well maintained lighthouse close to St. Martins.
                 </small>
               </div>
               <div class="card__footer">
-                <button class="button button--primary button--block">Visit</button>
+                <button class="button button--primary button--block">
+                  Visit
+                </button>
               </div>
             </div>
           </div>
@@ -49,157 +51,156 @@ function News(){
               <div class="card__body">
                 <h4>Quaco Lighthouse</h4>
                 <small>
-                  The Quaco Head Lighthouse is a well maintained lighthouse close to St.
-                  Martins. It is a short, beautiful walk to the lighthouse along the
-                  seashore.
+                  The Quaco Head Lighthouse is a well maintained lighthouse
+                  close to St. Martins. It is a short, beautiful walk to the
+                  lighthouse along the seashore.
                 </small>
               </div>
               <div class="card__footer">
-                <button class="button button--primary button--block">Visit</button>
+                <button class="button button--primary button--block">
+                  Visit
+                </button>
               </div>
             </div>
           </div>
         </div>
-        <div class="col col--4">
-          nothing
-        </div>
+        <div class="col col--4">nothing</div>
       </div>
     </section>
-  )
+  );
 }
 
 function Competition() {
-  return(
+  return (
     <section class="">
       <div class="row">
-        <div class="col col--6">
-          <h2 class="">Competition</h2>
-          <p>Leverage agile frameworks to provide a robust synopsis for high 
-            level overviews. Iterative approaches to corporate strategy foster 
-            collaborative thinking to further the overall value proposition. 
-            Organically grow the holistic world view of disruptive innovation 
-            via workplace diversity and empowerment.</p>
+        <div class="col col--6 centered">
+          <h2 class="">COMPETITION</h2>
+          <p class="text--justify">
+            Take your skill to the next level with challenges that put you up
+            against the best in the field. Apply your skills building defensive
+            networks, or be offensive and attack them. Our CCDC team will train
+            you from the ground up.
+          </p>
         </div>
         <div class="col col--6">
-          <img
-            src={useBaseUrl('img/nationals.jpg')}            
-            alt="Competition"
-          />
+          <img src={useBaseUrl("img/nationals.jpg")} alt="Competition" />
         </div>
       </div>
     </section>
-  )
+  );
 }
 
 function Event() {
-  return(
+  return (
     <section class="">
       <div class="row">
         <div class="col col--6">
-          <img
-            src={useBaseUrl('img/tech_symposium.jpg')}            
-            alt="Event"
-          />
+          <img src={useBaseUrl("img/tech_symposium.jpg")} alt="Event" />
         </div>
-        <div class="col col--6">
-          <h2 class="">Events</h2>
-          <p>Leverage agile frameworks to provide a robust synopsis for high 
-            level overviews. Iterative approaches to corporate strategy foster 
-            collaborative thinking to further the overall value proposition. 
-            Organically grow the holistic world view of disruptive innovation 
-            via workplace diversity and empowerment.</p>
+        <div class="col col--6 centered">
+          <h2 class="">EVENTS</h2>
+          <p class="text--justify">
+            We hold basic and advanced weekly workshops cover topics such as
+            using Linux, internet of things, and network forensics, an annual
+            on-campus, student-run conference, Tech Symposium, and a quarterly
+            LAN party, Frag Nite, held in conjunction with CPP E-Sports.
+          </p>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
 function Membership() {
-  return(
+  return (
     <section class="">
       <div class="row">
-        <div class="col col--6">
-          <h2 class="">Membership</h2>
-          <p>Leverage agile frameworks to provide a robust synopsis for high 
-            level overviews. Iterative approaches to corporate strategy foster 
-            collaborative thinking to further the overall value proposition. 
-            Organically grow the holistic world view of disruptive innovation 
-            via workplace diversity and empowerment.</p>
+        <div class="col col--6 centered">
+          <h2 class="">MEMBERSHIP</h2>
+          <p class="text--justify">
+            If you like learning new topics, solving problems, breaking things,
+            being with fellow hackers, get involved! Our membership provides you
+            with resources to enhance your college experience and your
+            professional outlook
+          </p>
         </div>
         <div class="col col--6">
-          <img
-            src={useBaseUrl('img/membership.jpg')}            
-            alt="Membership"
-          />
+          <img src={useBaseUrl("img/membership.jpg")} alt="Membership" />
         </div>
       </div>
     </section>
-  )
+  );
 }
 
 function Meeting() {
-  return(
+  return (
     <section class="">
       <div class="row">
         <div class="col col--6">
-          <img
-            src={useBaseUrl('img/meeting.png')}
-            alt="Meetings"
-          />
+          <img src={useBaseUrl("img/meeting.png")} alt="Meetings" />
         </div>
-        <div class="col col--6">
-          <h2 class="">Meetings</h2>
-          <p>Leverage agile frameworks to provide a robust synopsis for high 
-            level overviews. Iterative approaches to corporate strategy foster 
-            collaborative thinking to further the overall value proposition. 
-            Organically grow the holistic world view of disruptive innovation 
-            via workplace diversity and empowerment.</p>
+        <div class="col col--6 centered">
+          <h2 class="">MEETINGS</h2>
+          <p class="text--justify">
+            Come to our weekly meetings where we present and discuss topics
+            focusing on cyber security, networking, system administration, and
+            automation. We host peers and industry leaders in the field to give
+            our members an insight to the professional world of technology.
+          </p>
         </div>
       </div>
     </section>
-  )
+  );
 }
-
-
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
       title={`Welcome to ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-
-      <header className={clsx('hero hero--primary hero-banner')}>
+      description="Description will go into a meta tag in <head />"
+    >
+      <header className={clsx("hero hero--primary hero-banner")}>
         <div className="container hero-text">
           <div class="row">
             <div class="col col--6 centered">
               {/* <h1 className="hero__title">{siteConfig.title}</h1> */}
               {/* <p className="hero__subtitle">{siteConfig.tagline}</p> */}
               <img
-                src={useBaseUrl('img/logo_words_white.png')}
+                src={useBaseUrl("img/logo_words_white.png")}
                 alt="Image alt text"
                 title="Logo Title Text 1"
               />
               <div className={styles.buttons}>
                 <Link
                   className={clsx(
-                    'button button--outline button--secondary button--lg',
-                    styles.getStarted,
+                    "button button--outline button--secondary button--lg",
+                    styles.getStarted
                   )}
-                  to={useBaseUrl('about/')}>
+                  to={useBaseUrl("about/")}
+                >
                   Our Story
                 </Link>
               </div>
             </div>
             <div class="col col--6">
-              <a class="twitter-timeline" data-lang="en" 
-                data-width="300" data-height="450" 
-                data-theme="dark" 
-                href="https://twitter.com/calpolyswift?ref_src=twsrc%5Etfw">
-                  Tweets by calpolyswift</a>
-              <script async src="https://platform.twitter.com/widgets.js" 
-                charset="utf-8"></script>
+              <a
+                class="twitter-timeline"
+                data-lang="en"
+                data-width="300"
+                data-height="450"
+                data-theme="dark"
+                href="https://twitter.com/calpolyswift?ref_src=twsrc%5Etfw"
+              >
+                Tweets by calpolyswift
+              </a>
+              <script
+                async
+                src="https://platform.twitter.com/widgets.js"
+                charset="utf-8"
+              ></script>
             </div>
           </div>
         </div>
