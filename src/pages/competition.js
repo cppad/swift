a90e01d995bc77373a2b6f5127092d92a42e66b4
Michelle Lee <michellechulee@gmail.com>
2021-02-23 13:38:41 -0800

    layout complete

--- src/pages/competition.js
+++ src/pages/competition.js
@@ -1,14 +1,13 @@
-import React from "react";
-import clsx from "clsx";
-import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import React from 'react';
+import Layout from '@theme/Layout';
 import useBaseUrl from "@docusaurus/useBaseUrl";
-import styles from "./styles.module.css";
+import Link from "@docusaurus/Link";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faDatabase, faHandshake, faShieldAlt} from '@fortawesome/free-solid-svg-icons'
+import clsx from 'clsx';
+import styles from "./styles.module.css";
+import {faDatabase, faHandshake, faShieldAlt} from '@fortawesome/free-solid-svg-icons';
 
-function CoreValues() {
+function ThreeColumn() {
   return (
     <div class={clsx(styles.sectionPadding)}>
       <section class="container">
@@ -16,23 +15,21 @@ function CoreValues() {
           <div class="col col--4 centered">
             <FontAwesomeIcon icon={faHandshake} className={clsx(styles.sectionIcon)}/>
             <div class={clsx("padding--sm centered")}>
-              <h2>Networking</h2>
+              <h2>???</h2>
               <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
             </div>
           </div>
           <div class="col col--4 centered">
-            {/* <FontAwesomeIcon icon={faShieldAlt} class={clsx("core-icons")}/> */}
             <FontAwesomeIcon icon={faShieldAlt} className={clsx(styles.sectionIcon)}/>
             <div class={clsx("padding--sm centered")}>
-              <h2>Cybersecurity</h2>
+              <h2>???</h2>
               <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
             </div>
           </div>
           <div class="col col--4 centered">
-            {/* <FontAwesomeIcon icon={faDatabase} class={clsx("core-icons")} /> */}
             <FontAwesomeIcon icon={faDatabase} className={clsx(styles.sectionIcon)}/>
             <div class={clsx("padding--sm centered")}>
-              <h2>System Admin</h2>
+              <h2>???</h2>
               <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
             </div>
           </div>
@@ -42,13 +39,33 @@ function CoreValues() {
   );
 }
 
-function Competition() {
+function Description() {
   return (
-    <div class={clsx(styles.sectionPadding, "section-background-color")}>
+    <div class={styles.sectionPadding, "section-background-color"}>
+      <div class="container">
+        <div class={clsx("margin-centered padding--xl")}>
+          <p>
+            <span class={styles.firstWord}>Students</span> with an Interest in
+            the Future of Technology is the premier information technology and
+            security organization at Cal Poly Pomona. We aim to inspire and
+            create a generation of students who are aware and capable of dealing
+            with the dynamic landscape of information security, cyber security,
+            and information technology. Together we can build a secure future
+            through training, experience, and education.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function CPTC() {
+  return (
+    <div class={clsx(styles.sectionPadding)}>
       <section class="container">
         <div class="row">
           <div class="col col--5 centered padding--lg">
-            <h2 class="">Competition</h2>
+            <h2 class=""><a href="https://globalcptc.org/">CPTC</a></h2>
             <p class="text--justify">
               Take your skill to the next level with challenges that put you up
               against the best in the field. Apply your skills building defensive
@@ -57,7 +74,7 @@ function Competition() {
             </p>
           </div>
           <div class="col col--7 fixed-img">
-            <img src={useBaseUrl("img/trophies.jpg")} alt="Competition" />
+            <img src={useBaseUrl("img/cptc.jpg")} alt="Competition" />
           </div>
         </div>
       </section>
@@ -65,16 +82,16 @@ function Competition() {
   );
 }
 
-function Event() {
+function CCDC() {
   return (
-    <div class={clsx(styles.sectionPadding)}>
+    <div class={clsx(styles.sectionPadding, "section-background-color")}>
       <section class="container">
         <div class="row">
           <div class="col col--7 fixed-img">
-            <img src={useBaseUrl("img/tech_symposium.jpg")} alt="Event" />
+            <img src={useBaseUrl("img/ccdc.jpg")} alt="Event" />
           </div>
           <div class="col col--5 centered padding--lg">
-            <h2 class="">Events</h2>
+            <h2 class=""><a href="http://www.nationalccdc.org/">CCDC</a></h2>
             <p class="text--justify">
               We hold basic and advanced weekly workshops cover topics such as
               using Linux, internet of things, and network forensics, an annual
@@ -88,13 +105,15 @@ function Event() {
   );
 }
 
-function Membership() {
+function NCL() {
   return (
-    <div class={clsx(styles.sectionPadding, "section-background-color")}>
+    <div class={clsx(styles.sectionPadding)}>
       <section class="container">
         <div class="row">
           <div class="col col--5 centered padding--lg">
-            <h2 class="">Membership</h2>
+            <h2 class=""><a href="">National Cyber League</a></h2>
+          </div>
+          <div class="col col--7">
             <p class="text--justify">
               If you like learning new topics, solving problems, breaking things,
               being with fellow hackers, get involved! Our membership provides you
@@ -102,25 +121,18 @@ function Membership() {
               professional outlook
             </p>
           </div>
-          <div class="col col--7 fixed-img">
-            <img src={useBaseUrl("img/membership.jpg")} alt="Membership" />
-          </div>
         </div>
       </section>
     </div>
   );
 }
 
-function Meeting() {
+function Hive() {
   return (
-    <div class={clsx(styles.sectionPadding)}>
+    <div class={clsx(styles.sectionPadding, "section-background-color")}>
       <section class="container">
         <div class="row">
-          <div class="col col--7 fixed-img">
-            <img src={useBaseUrl("img/meeting.png")} alt="Meetings" />
-          </div>
-          <div class="col col--5 centered padding--lg">
-            <h2 class="">Meetings</h2>
+          <div class="col col--7">
             <p class="text--justify">
               Come to our weekly meetings where we present and discuss topics
               focusing on cyber security, networking, system administration, and
@@ -128,63 +140,38 @@ function Meeting() {
               our members an insight to the professional world of technology.
             </p>
           </div>
+          <div class="col col--5 centered padding--lg">
+            <h2 class=""><a href="http://hivestorm.org/rules.html">Hivestorm</a></h2>
+          </div>
         </div>
       </section>
     </div>
   );
 }
 
-function Home() {
-  const context = useDocusaurusContext();
-  const { siteConfig = {} } = context;
-  return (
-    <Layout
-      title={`Welcome to ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
-      {/* hero hero--primary hero-banner opacity*/}
-      <header class={clsx("hero-container")}>
-        <img
-          class="hero-image"
-          src={useBaseUrl('img/banner.jpg')}
-          alt="Image alt text"
-          title="Logo Title Text 1"
-        />
-        <div class="container hero-text">
-          <div class="row">
-            <div class="col col--12 centered hero-banner">
-              {/* <h1 class="hero__title">{siteConfig.title}</h1> */}
-              {/* <p class="hero__subtitle">{siteConfig.tagline}</p> */}
-              <img
-                src={useBaseUrl("img/logo_words_white.png")}
-                alt="Image alt text"
-                title="Logo Title Text 1"
-              />
-              <div class={styles.buttons}>
-                <Link
-                  class={clsx(
-                    "button button--outline button--secondary button--lg",
-                    styles.getStarted
-                  )}
-                  to={useBaseUrl("about/")}
-                >
-                  Our Story
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
 
+function Membership() {
+  return (
+    <Layout title="Hello">
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '18vh',
+        }}>
+        <h1>Cybersecurity Competitions</h1>
+      </div>
       <main>
-        <CoreValues />
-        <Competition />
-        <Event />
-        <Membership />
-        <Meeting />
+        <ThreeColumn />
+        <Description />
+        <CPTC />
+        <CCDC />
+        <NCL />
+        <Hive />
       </main>
     </Layout>
   );
 }
-
-export default Home;
+  
+export default Membership;
